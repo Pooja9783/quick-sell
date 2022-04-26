7174f097e178bc893aa9f2ee0316e1b92f588699
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-04-26 14:59:12 +0530

    implemented counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { useState } from "react";
 import "../Style/Counter.css";
 const Counter = () => {
-  let [counter, setCounter] = useState(1);
+  let [counter, setCounter] = useState(10);
+  //   console.log(input);
+
+  const counterVal = (val) => {
+    if (counter == "") {
+      setCounter(1000);
+    } else {
+      setCounter(val);
+    }
+  };
 
   return (
     <div className="counter-app">
       <div className="counter">
-        <input type="text" placeholder="Enter Number..." />
         <div className="counter-border">
           <button onClick={() => setCounter(counter - 1)}>-</button>
           <h1 className="h1">{counter}</h1>
-          <button onClick={() => setCounter(counter + 1)}>+</button>
+          <button onClick={() => counterVal(counter + 1)}>+</button>
         </div>
       </div>
     </div>
