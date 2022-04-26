26f4a9f8ec4e3f8e95ee54f30154a484d4d66c76
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-04-26 16:13:12 +0530

    implemented counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,30 +2,38 @@ import React from "react";
 import { useState } from "react";
 import "../Style/Counter.css";
 const Counter = () => {
-  //   let [counter, setCounter] = useState(10);
-  let [input, setInput] = useState(10);
-  const counterVal = (val) => {
-    if (input == " ") {
-      setInput(1);
-    } else if (input === 1000) {
-      setInput(val + 1);
-    } else {
-      setInput(val + 1);
-    }
-  };
+  let [counter, setCounter] = useState(1);
+  let [max, setMax] = useState(1000);
+
+  //   const maxVal = (val) => {
+  //     if (counter < 1000) {
+  //       setMax(1);
+  //     }
+  //   };
 
   return (
     <div className="counter-app">
       <div className="counter">
         <div className="counter-border">
-          <button onClick={() => setInput(input - 1)}>-</button>
-          <input
-            tpye="text"
-            onChange={(e) => setInput(e.target.value)}
-            value={+input}
-          />
-          <button onClick={() => counterVal(Number(input))}>+</button>
+          <button onClick={() => (counter > 1 ? setCounter(counter - 1) : 1)}>
+            -
+          </button>
+          <p>{counter}</p>
+          <button onClick={() => (counter < max ? setCounter(counter + 1) : 1)}>
+            +
+          </button>
         </div>
+        <input
+          tpye="Number"
+          onChange={(e) => setCounter(Number(e.target.value))}
+          placeholder="Enter Number...."
+        />
+        <br />
+        <input
+          tpye="Number"
+          onChange={(e) => setMax(Number(e.target.value))}
+          placeholder="Enter Max Number...."
+        />
       </div>
     </div>
   );
